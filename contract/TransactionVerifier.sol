5c931fabe697fff895ec40d7b68ef4870fa96110
SOMESH BANERJEE <54861461+somesh-banerjee@users.noreply.github.com>
2022-05-22 01:42:42 +0530

    Uodate contract

--- contract/TransactionVerifier.sol
+++ contract/TransactionVerifier.sol
@@ -7,7 +7,14 @@ contract TransactionVerifier{
         uint256 amount;
         uint256 time;
     }
+    struct oTransaction{
+        string paymentID;
+        string ipfs;
+        bytes32 hash;
+        bool present;
+    }
     mapping(uint256 => cTransaction) public ctransactions;
+    mapping(string => oTransaction) public otransactions;
     mapping(address => uint256) public userTransactionCount;
     address public owner;
     uint256 cTc;
@@ -25,11 +32,13 @@ contract TransactionVerifier{
         return cTc;
     }
 
-    function addTransaction() public {
-
+    function addTransaction(string memory _paymentId, string memory _ipfs) public {
+        bytes32 _hash = keccak256(abi.encode(_paymentId, _ipfs));
+        oTransaction memory newObj = oTransaction(_paymentId,_ipfs,_hash,true);
+        otransactions[_paymentId] = newObj;
     }
 
-    function verifyTransaction(address _sender, address _receiver, uint256 _amount, uint256 _time) public view returns(bool){
+    function verifycTransaction(address _sender, address _receiver, uint256 _amount, uint256 _time) public view returns(bool){
         bool res = false;
         for(uint256 i=1;i<=cTc;i++){
             res = ctransactions[i].from == _sender && ctransactions[i].to == _receiver && ctransactions[i].amount == _amount && ctransactions[i].time == _time;
@@ -37,5 +46,9 @@ contract TransactionVerifier{
                 break;
         }
         return res;
+    }
+
+    function verifyoTransaction(string memory _paymentId) public view returns(bool){
+        return otransactions[_paymentId].present;
     }
 }
