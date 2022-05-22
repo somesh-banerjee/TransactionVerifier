fc88bd0b885150824c503287c0468487225eec82
SOMESH BANERJEE <54861461+somesh-banerjee@users.noreply.github.com>
2022-05-22 08:41:40 +0530

    verify Online tx by Payment ID

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import NCT from './components/newCryptoTransaction'
 import VCT from './components/verifyCryptoTransaction'
+import NOT from './components/newOnlineTransaction'
+import VOT from './components/verifyOnlineTransaction'
 
 function App() {
   return (
@@ -8,6 +10,9 @@ function App() {
       <div className='crypto-tx'>
         <NCT />
         <VCT />
+      </div>
+      <div className='online-tx'>
+        <VOT />
       </div>
     </div>
   );
--- frontend/src/components/verifyOnlineTransaction.js
+++ frontend/src/components/verifyOnlineTransaction.js
@@ -1,7 +1,7 @@
-import React, { useState,useEffect } from "react";
+import React, { useState } from "react";
 import { ethers } from 'ethers'
 import Web3Modal from 'web3modal'
-import { Button, Radio, Item, Form } from "semantic-ui-react";
+import { Button, Form, Message } from "semantic-ui-react";
 
 import {
   contractAddress
@@ -11,11 +11,44 @@ import {
 import ABI from "./../web3/abi.json"
 
 function App() {
-    
+  const [paymentId,setPaymentId] = useState(null)
+  const [res,setRes] = useState()
+  const [showres,setShowRes] = useState(false)
+
+  const submit = async(e) => {
+      e.preventDefault()
+      if(paymentId === null)
+        return
+
+      setShowRes(false)        
+      const web3Modal = new Web3Modal()
+      const connection = await web3Modal.connect()
+      const provider = new ethers.providers.Web3Provider(connection)
+      const signer = provider.getSigner()
+      const Contract = new ethers.Contract(contractAddress, ABI, signer)
+      try {
+          let transaction = await Contract.verifyoTransaction(paymentId)
+          console.log(transaction)
+          setRes(transaction)
+          setShowRes(true)
+      } catch (er) {
+          console.log(er)
+      }
+  }
 
     return (
-      <div className="new-c-tx">
-
+      <div className="verify-o-tx">
+        <h1>Verify Online Transaction</h1>
+        <Form>
+            <Form.Field>
+                <label>Payment ID</label>
+                <input placeholder='PaymentId' onChange={(e) => setPaymentId(e.target.value)} />
+            </Form.Field>
+            <Button type='submit' onClick={submit}>Verify Transaction</Button>
+        </Form>
+        {
+          showres && (res ? <Message success> The Transaction is valid</Message> : <Message error> The Transaction is not valid</Message>)
+        }
       </div>
     );
   }
