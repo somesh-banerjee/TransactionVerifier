599e8ac13bec6961d494103343276cab2c68cd1d
SOMESH BANERJEE <54861461+somesh-banerjee@users.noreply.github.com>
2022-05-23 23:15:40 +0530

    Title change

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,4 @@
 //In Mumbai
-export const contractAddress = "0x720eE50ad4b8F923Af6E1F6E059925B4Dd481d8d"// process.env.REACT_APP_CONTRACT_ADDRESS;
+export const contractAddress = "0x720eE50ad4b8F923Af6E1F6E059925B4Dd481d8d" // process.env.REACT_APP_CONTRACT_ADDRESS;
 
  
